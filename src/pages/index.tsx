6778675ca9d9b5cc0d8439c9789e653e12419efd
Carine Batista <carinedcb@hotmail.com>
2021-02-24 09:43:10 -0300

    Add ChallengeBox

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,32 +1,32 @@
+import Head from 'next/head';
+
 import { CompletedChallenges } from "../components/CompletedChallenges";
 import { Countdown } from "../components/Countdown";
 import { ExperienceBar } from '../components/ExperienceBar';
 import {Profile} from "../components/Profile";
-
-import Head from 'next/head';
+import { ChallengeBox } from "../components/ChallengeBox";
 
 import styles from '../styles/pages/Home.module.css'
 
 export default function Home() {
   return (
-  
     <div className={styles.container}>
-    <Head>
-      <title>Inicio | Move.it </title>
-    </Head>
+      <Head>
+        <title>Inicio | Move.it </title>
+      </Head>
 
-    <ExperienceBar/>
+      <ExperienceBar/>
 
-    <section>
-      <div>
-        <Profile/>
-        <CompletedChallenges/>
-        <Countdown/>
-      </div>
-      <div>
-
-      </div>
-    </section>
-  </div>
+      <section>
+        <div>
+          <Profile/>
+          <CompletedChallenges/>
+          <Countdown/>
+        </div>
+        <div>
+          <ChallengeBox/>
+        </div>
+      </section>
+    </div>
   )
 }
